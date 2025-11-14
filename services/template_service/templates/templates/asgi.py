1a27612af7808996e8213485fcf870fa238163c5
temmy <favouradebose@gmail.com>
2025-11-14 12:47:52 +0100

    fix: correct .env variable naming for template_service

--- services/template_service/templates/templates/asgi.py
+++ services/template_service/templates/templates/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templates.settings')
+os.environ.setdefault('TEMPLATES_DJANGO_SETTINGS_MODULE', 'templates.settings')
 
 application = get_asgi_application()

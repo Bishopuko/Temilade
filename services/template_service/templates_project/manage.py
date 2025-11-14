2666dc3b9c83333eb7db170abb07c244b533b313
temmy <favouradebose@gmail.com>
2025-11-14 14:28:39 +0100

    fix: add correct path to start command in docker file for template service

--- services/template_service/templates_project/manage.py
+++ services/template_service/templates_project/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('TEMPLATES_DJANGO_SETTINGS_MODULE', 'templates.settings')
+    os.environ.setdefault('TEMPLATES_DJANGO_SETTINGS_MODULE', 'templates_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- services/template_service/templates_project/templates_project/wsgi.py
+++ services/template_service/templates_project/templates_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('TEMPLATES_DJANGO_SETTINGS_MODULE', 'templates.settings')
+os.environ.setdefault('TEMPLATES_DJANGO_SETTINGS_MODULE', 'templates_project.settings')
 
 application = get_wsgi_application()
